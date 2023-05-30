2bc9e7daae0e45768d978f2c10f7a3c89072d5e8
YukiOshino <131780334+YukiOshino@users.noreply.github.com>
2023-05-30 15:16:10 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 let playerImages = []; // プレイヤーの画像配列
 let currentImageIndex = 0; // 現在の画像のインデックス
 let player;
+let jumpCount = 0;
+let jumpCounts = [1, 3, 2, 2,];
+// アイテムの関数
+let items = [];
 // タイマーの関数
 let record;
 let t = 0;
@@ -15,7 +19,11 @@ let sabakuImg1
 let sabotenImg;
 let sunImg;
 let angle = 0;
-
+// ビーチ背景の関数
+let beachImg;
+// 森背景の関数
+let forestImg;
+let back_states = "";
 
 // スタート画面の描画
 function drawStartScreen() {
@@ -34,7 +42,51 @@ function drawStartScreen() {
   textSize(24);
   textAlign(CENTER, CENTER);
   text("Start", 400, 400);
-
+  //現在選択している動物を描画
+  imageMode(CENTER);
+  image(playerImages[currentImageIndex], 600, 400, 80, 60);
+  textSize(16);
+  text("Click to select animal\n↓", 600, 340);
+  if(currentImageIndex === 0) {
+    textSize(12);
+    text("馬\n連続ジャンプ可能回数：1回\nジャンプ力：高い", 600, 460);
+  }else if(currentImageIndex === 1) {
+    textSize(12);
+    text("亀\n連続ジャンプ可能回数：3回\nジャンプ力：低い", 600, 460);
+  }else if(currentImageIndex === 2) {
+    textSize(12);
+    text("チーター\n連続ジャンプ可能回数：2回\nジャンプ力：とても高い", 600, 460);
+  }else if(currentImageIndex === 3) {
+    textSize(12);
+    text("猫\n連続ジャンプ可能回数：2回\nジャンプ力：普通", 600, 460);
+  }
+  // 砂漠ボタンの四角の図形を描画
+  stroke(255);
+  noFill();
+  rect(200, 250, 100, 50);
+  // 背景（砂漠）ボタンのテキストを描画
+  noStroke();
+  fill(255);
+  textSize(20);
+  text("砂漠", 200, 250);
+  // 海ボタンの四角の図形を描画
+  stroke(255);
+  noFill();
+  rect(200, 350, 100, 50);
+  // 背景（海）ボタンのテキストを描画
+  noStroke();
+  fill(255);
+  textSize(20);
+  text("海", 200, 350);
+  // 森ボタンの四角の図形を描画
+  stroke(255);
+  noFill();
+  rect(200, 450, 100, 50);
+  // 背景（森）ボタンのテキストを描画
+  noStroke();
+  fill(255);
+  textSize(20);
+  text("森", 200, 450);
   imageMode(CENTER);
   image(playerImages[currentImageIndex], 600, 400, 80, 60);
 }
@@ -46,7 +98,6 @@ function drawGameOverScreen() {
   fill(255);
   textSize(32);
   text("Game Over", width / 2, height / 2 - 50);
-
 
   // Retryボタンの四角の図形を描画
   rectMode(CENTER);
@@ -79,8 +130,11 @@ function mouseClicked() {
   if (mouseX >= 300 && mouseX <= 500) {
     if (mouseY >= 375 && mouseY <= 425) {
       if (gameState === "start" || gameState === "gameover") {
+        song3.play();
+        if(back_states == "sabaku" || back_states == "forest" || back_states == "sea") {
         gameState = "game";
         resetGame();
+        }
       }
     }
   }
@@ -88,6 +142,7 @@ function mouseClicked() {
   if (mouseX >= 300 && mouseX <= 500) {
     if (mouseY >= 375 && mouseY <= 425) {
       if (gameState === "gameover") {
+        song3.play();
         gameState = "game";
         resetGame();
       }
@@ -97,6 +152,7 @@ function mouseClicked() {
   if (mouseX >= 300 && mouseX <= 500) {
     if (mouseY >= 425 && mouseY <= 475) {
       if (gameState === "gameover") {
+        song3.play();
         gameState = "start";
       }
     }
@@ -105,14 +161,40 @@ function mouseClicked() {
   if (mouseX >= 560 && mouseX <= 640) {
     if (mouseY >= 375 && mouseY <= 425) {
       if (gameState === "start") {
+        song3.play();
         nextImage();
       }
     }
   }
+  // 砂漠ボタンの範囲内でクリックされたか判定
+  if (mouseX >= 150 && mouseX <= 250) {
+    if (mouseY >= 225 && mouseY <= 275) {
+      if (gameState === "start") {
+        song3.play();
+        back_states = "sabaku";
+      }
+    }
+  }
+  // 海ボタンの範囲内でクリックされたか判定
+  if (mouseX >= 150 && mouseX <= 250) {
+    if (mouseY >= 325 && mouseY <= 375) {
+      if (gameState === "start") {
+        song3.play();
+        back_states = "sea";
+      }
+    }
+  }
+  // 森ボタンの範囲内でクリックされたか判定
+  if (mouseX >= 150 && mouseX <= 250) {
+    if (mouseY >= 425 && mouseY <= 475) {
+      if (gameState === "start") {
+        song3.play();
+        image(forestImg, width / 2, height / 2, 800,600);
+        back_states = "forest";
+      }
+    }
+  }
 }
-
-
-
 
 // ゲームオーバー判定
 function checkGameOver() {
@@ -193,9 +275,9 @@ function updatePosition(entity) {
     entity.y = highestBlock.posY - highestBlock.sizeY / 2 - 30;
     entity.vy = 0;
     entity.jumping = false;
+    jumpCount = 0;
   }
 }
-
 
 // プレイヤー作成
 function createPlayer() {
@@ -212,16 +294,21 @@ function createPlayer() {
 
 // 重力
 function applyGravity(entity) {
-  if(entity.loading == false) {
-   entity.vy += 0.15;
+  const gravity = [0.13, 0.4, 0.1, 0.2]; // 画像インデックスごとの重力の値
+  if (entity.loading == false) {
+    entity.vy += gravity[currentImageIndex]; // 重力を適用
   }
 }
 
 // ジャンプ
-function applyJump(entity) {
-  entity.vy = -5;
-  entity.jumping = true; // ジャンプ中フラグをtrueに設定
-  entity.rotation = 0; // 回転角度をリセット
+function applyJump(entity, n) {
+  const maxJumpCount = jumpCounts[currentImageIndex]; // 現在の画像インデックスに対応する連続ジャンプ可能回数を取得
+  if (jumpCount < maxJumpCount) { // 連続ジャンプ可能回数未満の場合のみジャンプを適用
+    entity.vy = n;
+    entity.jumping = true; // ジャンプ中フラグをtrueに設定
+    entity.rotation = 0; // 回転角度をリセット
+    jumpCount++; // 連続ジャンプ回数を増やす
+  }
 }
 
 //判定
@@ -235,7 +322,6 @@ function judgement(entity) {
     }
   }
 }
-
 
 // プレイヤー描画
 function drawPlayer(entity) {
@@ -283,12 +369,17 @@ function timer(entity) {
 function resetGame() {
   player = createPlayer();
   record = 0;
+  blocks = [];
+  frameCount = 0;
+  entity.loading = true;
+  timer(player);
+  updatePosition(player);
 }
 
 // マウスでジャンプ
 function mousePressed() {
   if (gameState === "game") {
-    applyJump(player);
+    applyJump(player, -5);
   }
 }
 
@@ -302,23 +393,28 @@ function nextImage() {
 
 // 砂漠背景の描画
 function sabaku() {
-
   image(sabakuImg, width / 2, height / 2, 800, 600);
   image(sabotenImg, 100, 300, 100, 100);
   image(sabotenImg, 400, 350, 100, 100);
-
   angle += 0.01;
   push();
   imageMode(CENTER);
-
   translate(650, 100);
   rotate(angle);
   image(sunImg, 0, 0, 150, 150);
   pop();
-
 }
 
+// ビーチ背景の描画
+function sea(){
+  image(beachImg, width / 2, height / 2, 800,600)
+  image(sunImg,700,10,150,150);
+}
 
+// 森背景の描画
+function forest(){
+  image(forestImg, width / 2, height / 2, 800,600);
+}
 
 function preload() {
   // 画像をロード
@@ -329,21 +425,40 @@ function preload() {
   sabakuImg = loadImage('sabaku.jpg');
   sabotenImg = loadImage('cactus.png');
   sunImg = loadImage('sun.png');
+  beachImg = loadImage('beach.jpg');
+  forestImg = loadImage('forest.jpeg');
+  hosiImage = loadImage('hosi.png');
+  isiImage = loadImage('isi.png');
+    //音楽の読み込み
+  
+    song1=createAudio('Short_8Bit_01.mp3');
+    song2=createAudio('ゆったりお散歩2-1.mp3');
+    song3=createAudio('選択2.mp3');
+    song4=createAudio('ファニージャンプ.mp3');
 }
 
 function setup() {
   createCanvas(800, 600); // 800 x 600 ピクセル。今回このサイズでやっていきます
   rectMode(CENTER); //四角形の基準点を中心に変更
   player = createPlayer();
+  song1.loop();
+  song2.loop();
 }
 
-
-
 function draw() {
-  sabaku();
   if (gameState === "start") {
     drawStartScreen();
+    song2.volume(0);
   } else if (gameState === "game") {
+    song1.volume(0);
+    song2.volume(1.0);
+      if(back_states == "forest") {
+        forest();
+      } else if(back_states == "sabaku") {
+        sabaku();
+      } else if(back_states == "sea") {
+        sea();
+      }
     judgement(player);
     updatePosition(player);
     applyGravity(player);
@@ -368,13 +483,46 @@ function draw() {
       blocks[i].move();
       blocks[i].display();
     }
+    if (frameCount % 120 === 0) { // 2秒ごとにアイテムを追加
+      items.push(new Item());
+    }
+
+    // アイテムの削除
+    for (let i = items.length - 1; i >= 0; i--) {
+      if (items[i].posX < -items[i].size) {
+        items.splice(i, 1);
+      }
+    }
+
+    // アイテムの移動と描画
+    for (let i = 0; i < items.length; i++) {
+      items[i].move();
+      items[i].display();
+
+      // プレイヤーとの当たり判定
+      if (
+        player.x + 40 > items[i].posX &&
+        player.x - 40 < items[i].posX + items[i].size &&
+        player.y + 30 > items[i].posY &&
+        player.y - 30 < items[i].posY + items[i].size
+      ) {
+        if (items[i].type === "obstacle") {
+          gameState = "gameover";
+        } else if (items[i].type === "powerup") {
+          jumpCount = 0;
+          applyJump(player, -7);
+          jumpCount = 0;
+        }
+        items.splice(i, 1); // アイテムを削除
+      }
+    }
   } else if (gameState === "gameover") {
     drawGameOverScreen();
     t = 0;
+    song1.volume(1.0);
+    song2.volume(0);
   }
 }
-
-
 
 class Block {
   constructor() {
@@ -392,5 +540,28 @@ class Block {
   display() {
     fill(0);
     rect(this.posX, this.posY, this.sizeX, this.sizeY);
+  }
+}
+
+class Item {
+  constructor() {
+    this.size = 30;
+    this.posX = width;
+    this.posY = random(height - this.size);
+    this.velocity = -3;
+    this.type = random(["obstacle", "powerup"]); // "obstacle"：ゲームオーバーになるアイテム, "powerup"：ジャンプ力が上がるアイテム
+  }
+
+  move() {
+    this.posX += this.velocity;
+  }
+
+  display() {
+    imageMode(CENTER);
+    if (this.type === "obstacle") {
+      image(isiImage, this.posX, this.posY, this.size, this.size);
+    } else if (this.type === "powerup") {
+      image(hosiImage, this.posX, this.posY, this.size, this.size);
+    }
   }
 }
